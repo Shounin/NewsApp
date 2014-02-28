ccbdceb0a8737ac7157136187a7f7d29cb020ebe
Garviell <faustusxi@gmail.com>
2014-02-28 18:15:20 +0000

    LOOK AT MAH CHANGES

---
+++ NewsApp/DAL/NewsContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+
+namespace NewsApp.DAL
+{
+    public class NewsContext : DbContext
+    {
+    }
+}
---
+++ NewsApp/Models/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsApp.Models
+{
+    public class News
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Text { get; set; }
+        public string Category { get; set; }
+        public DateTime PostDate { get; set; }
+    }
+}
