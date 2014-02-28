e6ad10b935113807e29accd665816752a0bc36cf
Garviell <faustusxi@gmail.com>
2014-02-28 21:59:18 +0000

    BLAWRDSDFS

--- NewsApp/App_Start/RouteConfig.cs
+++ NewsApp/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace NewsApp
 			routes.MapRoute(
 				name: "Default",
 				url: "{controller}/{action}/{id}",
-				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+				defaults: new { controller = "News", action = "Index", id = UrlParameter.Optional }
 			);
 		}
 	}
