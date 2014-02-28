c3414ed0ad7a64762f9d7971851ef65d61417e44
Garviell <faustusxi@gmail.com>
2014-02-28 20:53:12 +0000

    I MADE CREATE WORK BUT I BROKEDED EDIT

---
+++ NewsApp/Models/CategoriesList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace System.Web.Mvc
+{
+    public class cate
+    {
+        public string CatId { get; set; }
+        public string Name { get; set; }
+    }
+    public class CategoriesList
+    {
+        public static IEnumerable<cate> Items = new List<cate> {
+        new cate {
+            CatId = "Politics",
+            Name = "Politics"
+        },
+        new cate {
+            CatId = "News",
+            Name = "News"
+        },
+        new cate {
+            CatId = "Sports",
+            Name = "Sports"
+        },
+        new cate {
+            CatId = "Education",
+            Name = "Education"
+        }
+    };
+    }
+}
