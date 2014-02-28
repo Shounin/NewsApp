89ab8295fbc95b0c2273a670cc671407a988f94a
Garviell <faustusxi@gmail.com>
2014-02-28 18:48:23 +0000

    MADE CONTEXT AND INTITIALIZEERS AND SHIT

--- NewsApp/DAL/NewsContext.cs
+++ NewsApp/DAL/NewsContext.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NewsApp.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,5 +10,14 @@ namespace NewsApp.DAL
 {
     public class NewsContext : DbContext
     {
+        public NewsContext() : base("NewsContext")
+        { }
+
+        public DbSet<News> News { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
     }
 }
---
+++ NewsApp/DAL/NewsInitializer.cs
@@ -0,0 +1,27 @@
+﻿using NewsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NewsApp.DAL
+{
+    public class NewsInitializer :System.Data.Entity. DropCreateDatabaseIfModelChanges<NewsContext>
+    {
+        protected override void Seed(NewsContext context)
+        {
+            var news = new List<News>
+            {
+                new News{Title="HOLY FUCKSICLES ITS RAINING MEN", Text="There isn't really anything to add", Category="News", PostDate=DateTime.Now},
+                new News{Title="HOLY FUCKSICLES ITS RAINING MEN", Text="There isn't really anything to add", Category="News", PostDate=DateTime.Now},
+                new News{Title="HOLY FUCKSICLES ITS RAINING MEN", Text="There isn't really anything to add", Category="News", PostDate=DateTime.Now},
+                new News{Title="HOLY FUCKSICLES ITS RAINING MEN", Text="There isn't really anything to add", Category="News", PostDate=DateTime.Now},
+                new News{Title="HOLY FUCKSICLES ITS RAINING MEN", Text="There isn't really anything to add", Category="News", PostDate=DateTime.Now},
+                new News{Title="HOLY FUCKSICLES ITS RAINING MEN", Text="There isn't really anything to add", Category="News", PostDate=DateTime.Now},
+                new News{Title="HOLY FUCKSICLES ITS RAINING MEN", Text="There isn't really anything to add", Category="News", PostDate=DateTime.Now},
+            };
+            news.ForEach(s => context.News.Add(s));
+            context.SaveChanges();
+        }
+    }
+}
